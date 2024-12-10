bc1794aa42b4617846f11297ca49648e05fdc38c
Arezooshah <167054584+Arezooshah@users.noreply.github.com>
2024-12-10 16:38:13 +0330

    create a custom hook to fetch the games

---
+++ src/components/GameCard.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Game } from "../Hooks/useGames";
+interface Props {
+  game: Game;
+}
+const GameCard = () => {
+  return <div></div>;
+};
+
+export default GameCard;
--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,35 +1,17 @@
-import react, { useEffect, useState } from "react";
-import apiClient from "./Services/api-client";
 import { Text } from "@chakra-ui/react";
+import useGames from "../Hooks/useGames";
 const GameGrid = () => {
-  const [games, setGames] = useState<Game[]>([]);
-  const [error, setError] = useState(" ");
-
-  interface Game {
-    id: number;
-    name: string;
-  }
-
-  interface FetchGamesResponse {
-    count: number;
-    results: Game[];
-  }
-  useEffect(() => {
-    apiClient
-      .get<FetchGamesResponse>("/xgames")
-      .then((res) => setGames(res.data.results))
-      .catch((err) => setError(err.message));
-  });
+  const { games, error } = useGames();
 
   return (
-    <>
-      {error && <Text>{error}</Text>};
+    <div>
+      {/* {error && <Text>{error}</Text>}; */}
       <ul>
-        {games.map((items) => (
-          <li key={items.id}>{items.name}</li>
+        {games.map((game) => (
+          <li key={game.id}>{game.name}</li>
         ))}
       </ul>
-    </>
+    </div>
   );
 };
 
