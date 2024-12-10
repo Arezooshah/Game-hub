bf3a19aa0cc5368f033cdc93f0687efffc889b1f
Arezooshah <167054584+Arezooshah@users.noreply.github.com>
2024-12-10 10:40:29 +0330

    responsive

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,8 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { Provider } from "@/components/ui/provider";
 import "./index.css";
 import App from "./App.jsx";
+import { Provider } from "./components/ui/provider.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
