48bb0e48a4b24e32c04d334663dd0fff1d968d17
Arezooshah <167054584+Arezooshah@users.noreply.github.com>
2024-12-10 17:29:25 +0330

    building cart

--- src/Hooks/useGames.ts
+++ src/Hooks/useGames.ts
@@ -5,6 +5,7 @@ import apiClient from "../components/Services/api-client";
 export interface Game {
   id: number;
   name: string;
+  background_image: string;
 }
 
 interface FetchGamesResponse {
--- src/components/Services/api-client.ts
+++ src/components/Services/api-client.ts
@@ -3,6 +3,6 @@ import axios from "axios";
 export default axios.create({
   baseURL: "https://api.rawg.io/api",
   params: {
-    key: "e8012b324dbf4912b2ec80361cfd15d2",
+    key: "3ed624d8d88040a2af37c437b36ab4e6",
   },
 });
