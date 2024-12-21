9052c2d75722ab75fb471466d808a6cb8fe4e648
Arezooshah <167054584+Arezooshah@users.noreply.github.com>
2024-12-22 01:22:59 +0330

    build search

--- src/components/SearchInput.tsx
+++ src/components/SearchInput.tsx
@@ -1,10 +1,11 @@
-import { input } from "@chakra-ui/react";
+import { Input, InputGroup, InputLeftElement } from "@chakra-ui/react";
+import { BsSearch } from "react-icons/bs";
 const SearchInput = () => {
   return (
     <InputGroup>
       <InputLeftElement children={<BsSearch />} />
       <Input
-        ref={ref}
+        // ref={ref}
         borderRadius={20}
         placeholder="Search games..."
         variant="filled"
